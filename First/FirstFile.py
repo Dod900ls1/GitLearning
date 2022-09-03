15d3e93859687a9bbecb765698ffa8c98eb85efd
yehorboiar <boyaregor1@gmail.com>
2022-09-03 11:58:31 +0100

    init

---
+++ First/FirstFile.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+
+
+def RandDataFrame():
+    x = [[random.randint(0, 20) for _ in range(5)] for _ in range(5)]
+    x1 = pd.DataFrame(x)
+    print(x1)
+    return x
+
+
+if __name__ == "__main__":
+    RandDataFrame()
